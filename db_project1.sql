d69eea6b32a460d317f6904a17342571b584427c
alfigufron <alfigufron21@gmail.com>
2019-11-17 14:38:12 +0700

    problem-login

--- db_project1.sql
+++ db_project1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Nov 2019 pada 03.58
+-- Waktu pembuatan: 17 Nov 2019 pada 08.31
 -- Versi server: 10.4.6-MariaDB
 -- Versi PHP: 7.3.9
 
@@ -25,133 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_cart`
+-- Struktur dari tabel `tbl_admin`
 --
 
-CREATE TABLE `tbl_cart` (
-  `id` int(11) NOT NULL,
-  `code_user` int(3) NOT NULL,
-  `code_goods` int(3) NOT NULL,
-  `goods_name` varchar(64) NOT NULL,
-  `lots` int(3) NOT NULL,
-  `price` bigint(8) NOT NULL,
-  `note` text NOT NULL,
-  `date_entered` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_goods`
---
-
-CREATE TABLE `tbl_goods` (
-  `id` int(11) NOT NULL,
-  `code_goods` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `price` bigint(8) NOT NULL,
-  `stock` int(3) NOT NULL,
-  `note` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_purchase`
---
-
-CREATE TABLE `tbl_purchase` (
-  `id` int(11) NOT NULL,
-  `transaction_code` int(10) NOT NULL,
-  `user_code` int(10) NOT NULL,
-  `goods_code` int(10) NOT NULL,
-  `goods_name` varchar(255) NOT NULL,
-  `lots` int(3) NOT NULL,
-  `price` bigint(8) NOT NULL,
-  `note` text NOT NULL,
-  `shipping_address` text NOT NULL,
-  `transaction_date` date NOT NULL,
-  `purchase_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_transaction`
---
-
-CREATE TABLE `tbl_transaction` (
-  `id` int(11) NOT NULL,
-  `code` int(10) NOT NULL,
-  `user_code` int(10) NOT NULL,
-  `goods_code` int(10) NOT NULL,
-  `goods_name` varchar(255) NOT NULL,
-  `lots` int(3) NOT NULL,
-  `price` bigint(8) NOT NULL,
-  `note` text NOT NULL,
-  `shipping_address` text NOT NULL,
-  `proof` text NOT NULL,
-  `date_entered` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_user`
---
-
-CREATE TABLE `tbl_user` (
+CREATE TABLE `tbl_admin` (
   `id` int(11) NOT NULL,
   `code` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `birth` date NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `phone_number` varchar(14) NOT NULL,
-  `address` text NOT NULL,
-  `picture` text NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tbl_user`
+-- Dumping data untuk tabel `tbl_admin`
 --
 
-INSERT INTO `tbl_user` (`id`, `code`, `name`, `username`, `password`, `birth`, `email`, `phone_number`, `address`, `picture`) VALUES
-(4, 2147483647, 'M Alfi Gufron', 'user', '$2y$10$C/MWgjfybchmoSgYsfVjh..QqQz84mij6XCLQw4SKJG5mWHANifRa', '2019-11-01', 'alfigufron21@gmail.com', '089675247929', 'Bogor', '26user.jpg');
+INSERT INTO `tbl_admin` (`id`, `code`, `name`, `username`, `password`) VALUES
+(2, 12022003, 'Gufron', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(3, 120203, 'Gufron 2', 'admin1', 'admin1');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `tbl_cart`
+-- Indeks untuk tabel `tbl_admin`
 --
-ALTER TABLE `tbl_cart`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `tbl_goods`
---
-ALTER TABLE `tbl_goods`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `tbl_purchase`
---
-ALTER TABLE `tbl_purchase`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `tbl_transaction`
---
-ALTER TABLE `tbl_transaction`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `tbl_user`
---
-ALTER TABLE `tbl_user`
+ALTER TABLE `tbl_admin`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -159,34 +59,10 @@ ALTER TABLE `tbl_user`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_cart`
+-- AUTO_INCREMENT untuk tabel `tbl_admin`
 --
-ALTER TABLE `tbl_cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `tbl_goods`
---
-ALTER TABLE `tbl_goods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `tbl_purchase`
---
-ALTER TABLE `tbl_purchase`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `tbl_transaction`
---
-ALTER TABLE `tbl_transaction`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `tbl_user`
---
-ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `tbl_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
